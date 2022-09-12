0578029444195e3d8ee99056a44a3d61a7641ec5
XisiPizz <88344611+me-mozz@users.noreply.github.com>
2022-09-12 09:55:25 +0700

    Create mzwa.js

---
+++ mzwa.js
@@ -0,0 +1,21 @@
+{
+
+    "version": "7.0",
+
+    "download": "https://apkadmin.com/",
+
+    "update": [{
+
+        "languaje": "en",
+
+        "changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-en.html"
+
+    }, {
+
+        "languaje": "id",
+
+        "changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-es.html"
+
+    }]
+
+}
