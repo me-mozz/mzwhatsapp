23eba580ee2d4c972beefecf30277234d479764a
XisiPizz <88344611+me-mozz@users.noreply.github.com>
2022-09-12 10:52:17 +0700

    Update mzwa.js

--- mzwa.js
+++ mzwa.js
@@ -1,18 +1,18 @@
 {
 
-    "version": "27.0",
+    "version": "7.0",
 
     "download": "https://apkadmin.com/",
 
     "update": [{
 
-        "languaje": "en",
+        "bahasa": "en",
 
         "changelog": "https://raw.githubusercontent.com/me-mozz/mzwhatsapp/main/changelog/en/mzwa.html"
 
     }, {
 
-        "languaje": "in",
+        "bahasa": "in",
 
         "changelog": "https://raw.githubusercontent.com/me-mozz/mzwhatsapp/main/changelog/indonesia/mzwa.html"
 
